530e561225e469417d70fdd86125168d16bd73d6
LaNice Powell <lanicemp@aol.com>
2020-01-06 11:17:13 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  resources :songs
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'songs/destroy'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  resources :genres
   resources :artists, :genres, only: [:index, :show, :new, :create, :edit, :update]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200106142602) do
+ActiveRecord::Schema.define(version: 20200106153033) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.string   "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "artist_id"
+    t.string   "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
